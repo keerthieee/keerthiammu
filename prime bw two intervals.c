2c2177a39fa577307c81cbf062aea29dfdbaf389
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-12 11:55:33 +0530

    Update prime bw two intervals.c

--- prime bw two intervals.c
+++ prime bw two intervals.c
@@ -5,23 +5,19 @@ int main(void) {
 	scanf("%d%d",&a,&b);
 	for(i=a;i<b;i++)
 	{
+		sum=0;
 	for(j=1;j<b;j++)
 		{
 			if(i%j==0)
-			{
+			
 			sum++;
-			}
-	
+			
+	}
 		if(sum==2)
-{
-	printf("%d",i);
-}
-else
-{
 
-}
+	printf("%d",i);
+
 	}
-	}
+	
 	return 0;
 }
-
