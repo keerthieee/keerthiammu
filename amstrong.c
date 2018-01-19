fc1e79f5be6346b0c8e9637ee29d41ef66d871cd
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-19 15:52:16 +0530

    Update amstrong.c

--- amstrong.c
+++ amstrong.c
@@ -3,23 +3,23 @@
 int main(void) {
 int i,j,orino,r,result=0;
 scanf("%d",&i);
-i=orino;
+orino=i;
 while(i!=0)
 {
 	r=i%10;
 	result=result+r*r*r;
-i/10;
+i=i/10;
 	
 }
 if(orino==result)
-{
+
 	printf("it is armstrong");
 	
-}
+
 else
-{
+
 	printf("it is not armstrong");
 	
-}
+
 	return 0;
 }
