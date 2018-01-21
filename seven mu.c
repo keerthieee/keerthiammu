38049869c56e089f8a394599540e9899b7446b16
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-21 16:59:43 +0530

    Create seven mu.c

---
+++ seven mu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,c;
+scanf("%d",&a);
+
+
+if(c%7==0)
+{
+	printf("no is dividible by 7");
+}
+else
+{
+	printf("no is not dividible by 7");
+}
+return 0;
+
+}
