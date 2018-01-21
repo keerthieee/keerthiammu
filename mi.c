5edd2b03ee48990dbf4abbc22395f21030b6a35a
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-21 15:40:05 +0530

    Create mi.c

---
+++ mi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,a[i],j,k,n,l,temp=0,t=0;
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+ 	for(i=0;i<k;i++)
+{
+	for(l=i+1;l<k;l++)
+	{
+		if(a[i]<a[l])
+		{
+t=a[i];
+a[i]=a[l];
+a[l]=t;
+}
+}
+}
+printf("%d",t);
+	return 0;
+}
