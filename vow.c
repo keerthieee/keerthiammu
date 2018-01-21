f4252f06506ec6927f56b0d6ec724b92fd62693e
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-21 15:08:16 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+	char c;
+scanf("%c",&c);
+if(c=='a' || c=='e' || c=='i' || c=='o' ||c=='u')
+{
+printf("it is an vowels");
+}
+else
+{
+printf("it is an constant");
+	}
+return 0;
+}
