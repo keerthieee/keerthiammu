d3229ccb72bce2c4684e454f65fe135efe5a1bf3
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-23 15:50:38 +0530

    Create pr.c

---
+++ pr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int k,v,sum;
+	scanf("%d",&k);
+	
+	for(v=1;v<=i;v++)
+	{
+		
+			if(k%v==0)
+		{	
+			sum++;
+			
+		}
+	}
+		if(sum==2)
+		
+
+	printf("it is prime number");
+else
+
+printf("not a prime");
+
+
+	return 0;
+}
