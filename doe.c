228c8a4de3036a9acd1f9e58ccf657287c00d8a7
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-23 15:48:06 +0530

    Create doe.c

---
+++ doe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int k,v,c;
+scanf("%d%d",&a,&b);
+c=k-v;
+printf("%d\n",c);
+if(c%2==0)
+{
+	printf("no is even");
+}
+else
+{
+	printf("no is odd");
+}
+return 0;
+
+}
