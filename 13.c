812c535b6d569a28ca2360edb77119ecd16a7bfb
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-01-23 16:16:43 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,c;
+scanf("%d",&a);
+if(a%13==0)
+{
+	printf("no is dividible by 13");
+}
+else
+{
+	printf("no is not dividible by 13");
+}
+return 0;
+
+}
