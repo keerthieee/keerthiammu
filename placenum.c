f682ae39c5d8a0f8e3475e84640fdf40b21a95c4
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-03-01 16:04:05 +0530

    Create placenum.c

---
+++ placenum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+
+{
+	int i,a[i],j,k;
+	scanf("%d",&k);
+	scanf("%d",&j);
+	for(i=0;i<k;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+	printf("%d\t",a[j-1]);
+return 0;
+}
