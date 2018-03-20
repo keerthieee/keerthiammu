f761b5f83ac59217adc7b112a62d87dfacc8173a
keerthieee <31505484+keerthieee@users.noreply.github.com>
2018-03-20 15:51:34 +0530

    Create ->des.c

---
+++ ->des.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,a[i],j,k,n,l,temp=0,t=0;
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+      for(i=0;i<k;i++)
+{		
+	for(l=i+1;l<k;l++)
+	{
+		if(a[i]<a[l])
+		{
+temp=a[i];
+a[i]=a[l];
+a[l]=temp;
+}
+}
+}
+ for(i=0;i<k;i++)
+     {
+         if(i<=k-2)
+         {
+printf("%d->",a[i]);
+}
+else
+{
+printf("%d",a[i]);
+}
+}
+return 0;
+}
